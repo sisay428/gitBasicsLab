20eb5fd31a78ab741306ad8194b7c2a0bc47343f
sisay428 <sisayregassa@Sisays-MacBook-Pro.local>
2021-10-07 01:03:34 -0700

    last message

--- src/main/java/csc131/GiftCardApplication.java
+++ src/main/java/csc131/GiftCardApplication.java
@@ -7,10 +7,12 @@ public class GiftCardApplication {
 		GiftCard gc1= new GiftCard(25, 125.00);
 		GiftCard gc2= new GiftCard(50, 150.00);
 		GiftCard gc3= new GiftCard(75, 175.00);
+		GiftCard gc4= new GiftCard(85, 185.00);
 		ArrayList<GiftCard> giftCards= new ArrayList<GiftCard>();
 		giftCards.add(gc1);
 		giftCards.add(gc2);
 		giftCards.add(gc3);
+		giftCards.add(gc4);
 		int gcLength = giftCards.size();
 		for(int i=0; i< gcLength; i++) {
 			GiftCard gc= giftCards.get(i);
